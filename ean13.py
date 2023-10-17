3c1e4b823922b7aa65e20e7cb2c25deddef79fcf
santiago18 <santiago.sao@gmail.com>
2023-10-17 16:05:47 -0300

    Message that describes what this change does

---
+++ ean13.py
@@ -0,0 +1,28 @@
+"""
+Module to create and check EAN13 numbers.
+"""
+import random
+
+class Foo:
+    """Help for the class Foo"""
+
+    def bar(self):
+        """Help for the bar method of Foo classes"""
+
+def spam(f):
+    """Help for the spam function"""
+
+def greeting(name):
+  print("Hello, " + name)
+
+def func():
+  sal = ''.join([str(random.randint(0, 9)) for i in range(12)])
+  return sal
+
+def check_digit(digits):
+  cd = '0'
+  return cd
+
+def check_ean(digits):
+  cd = '0'
+  return cd

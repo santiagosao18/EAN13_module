6063c4b9f7d3cb079879fb89dbc98e4a365c5cb0
santiago18 <santiago.sao@gmail.com>
2023-10-17 16:32:04 -0300

    Completed the relevant functions.

--- ean13.py
+++ ean13.py
@@ -2,7 +2,11 @@
 Module to create and check EAN13 numbers.
 Otra parte.
 """
+
+# Imported libraries
 import random
+
+# Defined classes
 
 class Foo:
     """Help for the class Foo"""
@@ -16,19 +20,39 @@ def spam(f):
 def greeting(name):
   print("Hello, " + name)
 
+
+# Defined functions
+
+def check_digit(digits):
+  """
+  Computes the check digit from a sequence of 12 digits.
+  """
+  odd = sum([int(digits[i]) for i in range(len(digits)-1) if i%2 == 0])
+  even = 3 * sum([int(digits[i]) for i in range(len(digits)) if i%2 == 1])
+  cd = 10 - (odd + even) % 10
+  return str(cd)
+
 def rand_num():
   """
   Function that creates a random ean13 number.
   """
   sal = ''.join([str(random.randint(0, 9)) for i in range(12)])
+  cd = check_digit(sal)
+  sal = sal + cd
   return sal
 
-def check_digit(digits):
-  odd = sum([int(digits[i]) for i in range(len(digits)-1) if i%2 == 0])
-  even = 3 * sum([int(digits[i]) for i in range(len(digits)) if i%2 == 1])
-  cd = 10 - (odd + even) % 10
-  return str(cd), even, odd
-
 def check_ean(digits):
-  cd = '0'
-  return cd
+  """
+  Function that checks if a sequence of 13 digits is a correct ean13
+  number.
+  """
+  if len(digits) != 13:
+    return False
+  else:
+    if check_digit(digits[:12]) == digits[-1]:
+      return True
+    else:
+      return False
+
+
+
